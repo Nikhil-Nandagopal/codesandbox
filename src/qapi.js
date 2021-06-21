e2ad891f1a8ff9164254385154b62ae3ab7cfd0e
Nikhil Nandagopal <nikhil.nandagopal@gmail.com>
2021-06-21 17:49:07 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60d07305055903001fd690f7",
+				"60d07305055903001fd690f9"
+			]
+		},
+		"60d07305055903001fd690f7": {
+			"id": "60d07305055903001fd690f7",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60d07305055903001fd690f9": {
+			"id": "60d07305055903001fd690f9",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
